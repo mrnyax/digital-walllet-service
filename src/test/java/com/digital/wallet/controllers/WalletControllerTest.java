73fbeec60c0f50c04b61497287100d0b8e2d47ec
nyakoria <nyakoria@gmail.com>
2025-10-29 15:22:11 +0300

    test: mock wallet service

--- src/test/java/com/digital/wallet/controllers/WalletControllerTest.java
+++ src/test/java/com/digital/wallet/controllers/WalletControllerTest.java
@@ -1,15 +1,20 @@
 package com.digital.wallet.controllers;
 
-import com.digital.wallet.dtos.request.WalletDto;
+import com.digital.wallet.dtos.request.CreateWalletRequest;
+import com.digital.wallet.dtos.response.CreateWalletResponse;
+import com.digital.wallet.services.WalletService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,26 +31,34 @@ class WalletControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @MockitoBean
+    private WalletService walletService;
+
     @Test
     void createWallet_returnsStatusCreated() throws Exception {
+
+        when(walletService.createWallet(any())).thenReturn(new CreateWalletResponse(null));
+
         final UUID userId = UUID.randomUUID();
         final String walletName = "Test Wallet";
-        final WalletDto walletDto = new WalletDto(userId, walletName);
+        final CreateWalletRequest createWalletRequest = new CreateWalletRequest(userId, walletName);
         mockMvc.perform(
                         post(WALLET_ENDPOINT)
                                 .contentType(MediaType.APPLICATION_JSON)
-                                .content(objectMapper.writeValueAsString(walletDto))
+                                .content(objectMapper.writeValueAsString(createWalletRequest))
                 )
                 .andExpect(status().isCreated());
     }
 
     @Test
     void createWallet_whenInvalidRequestBody_returnsBadRequest() throws Exception {
-        final WalletDto walletDto = new WalletDto(null, null);
+        when(walletService.createWallet(any())).thenReturn(new CreateWalletResponse(null));
+
+        final CreateWalletRequest createWalletRequest = new CreateWalletRequest(null, null);
         mockMvc.perform(
                         post(WALLET_ENDPOINT)
                                 .contentType(MediaType.APPLICATION_JSON)
-                                .content(objectMapper.writeValueAsString(walletDto))
+                                .content(objectMapper.writeValueAsString(createWalletRequest))
                 )
                 .andDo(print())
                 .andExpect(status().isBadRequest())
