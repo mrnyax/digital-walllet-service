b83361d555287520fc51de9565d5a29c37a6e498
nyakoria <nyakoria@gmail.com>
2025-10-29 15:01:33 +0300

    refactor: changed property order

--- src/main/java/com/digital/wallet/dtos/request/WalletDto.java
+++ src/main/java/com/digital/wallet/dtos/request/WalletDto.java
@@ -6,9 +6,9 @@ import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 
 public record WalletDto(
-        @NotBlank(message = "Name is required.")
-        String name,
         @NotNull(message = "User ID is required")
-        UUID userId
+        UUID userId,
+        @NotBlank(message = "Name is required.")
+        String name
 ) {
 }
