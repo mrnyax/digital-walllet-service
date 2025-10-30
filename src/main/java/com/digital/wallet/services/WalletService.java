e679dc53029aacf670258ce36cd120406c976f83
nyakoria <nyakoria@gmail.com>
2025-10-30 16:50:19 +0300

    feat: add service method to handle wallrt credit

--- src/main/java/com/digital/wallet/services/WalletService.java
+++ src/main/java/com/digital/wallet/services/WalletService.java
@@ -1,11 +1,16 @@
 package com.digital.wallet.services;
 
 import com.digital.wallet.dtos.request.CreateWalletRequest;
+import com.digital.wallet.dtos.request.MoneyTrxRequestDto;
 import com.digital.wallet.dtos.response.CreateWalletResponse;
+import com.digital.wallet.dtos.response.MoneyTrxResponseDto;
 import com.digital.wallet.repositories.WalletRepository;
 import com.digital.wallet.repositories.entities.Wallet;
+import jakarta.validation.Valid;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class WalletService {
@@ -21,5 +26,10 @@ public class WalletService {
         final Wallet wallet = Wallet.of(createWalletRequest.userId(), createWalletRequest.name());
         final Wallet saved = walletRepository.save(wallet);
         return new CreateWalletResponse(saved.getOwner());
+    }
+
+    @Transactional
+    public MoneyTrxResponseDto credit(UUID id, @Valid MoneyTrxRequestDto req) {
+        return new MoneyTrxResponseDto();
     }
 }
