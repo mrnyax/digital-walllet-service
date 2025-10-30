82944b2ec9eebf5c87520d5f0c7f1d44a4906140
nyakoria <nyakoria@gmail.com>
2025-10-30 16:48:55 +0300

    feat: add endpoint to credit wallet

--- src/main/java/com/digital/wallet/controllers/WalletController.java
+++ src/main/java/com/digital/wallet/controllers/WalletController.java
@@ -1,7 +1,9 @@
 package com.digital.wallet.controllers;
 
 import com.digital.wallet.dtos.request.CreateWalletRequest;
+import com.digital.wallet.dtos.request.MoneyTrxRequestDto;
 import com.digital.wallet.dtos.response.CreateWalletResponse;
+import com.digital.wallet.dtos.response.MoneyTrxResponseDto;
 import com.digital.wallet.services.WalletService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -10,8 +12,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 
-import java.net.URI;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("wallets")
@@ -29,5 +33,13 @@ public class WalletController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(response);
+    }
+
+    @PostMapping("/{id}/credit")
+    public ResponseEntity<MoneyTrxResponseDto> credit(@PathVariable UUID id,
+                                                      @RequestHeader(value="Idempotency-Key", required=false) String key,
+                                                      @Valid @RequestBody MoneyTrxRequestDto req) {
+        final MoneyTrxResponseDto response = walletService.credit(id, req);
+        return ResponseEntity.ok(response);
     }
 }
